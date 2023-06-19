7ac79bfa41c380e21ed88b0ab13d3b3a01fe569f
Santiago <vazquezgrillo.santiago@gmail.com>
2023-06-19 17:31:38 -0300

    main.go convert temperature

--- ConvertTemperature/main.go
+++ ConvertTemperature/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/convert", convertHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func convertHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Mehotd not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
