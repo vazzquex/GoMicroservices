be0c0337170c5f712654c56ab8b7dd3fef4f422b
Santiago <vazquezgrillo.santiago@gmail.com>
2023-06-19 17:36:37 -0300

    set main.go

--- ConvertTemperature/main.go
+++ ConvertTemperature/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -15,4 +16,39 @@ func convertHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Mehotd not allowed", http.StatusMethodNotAllowed)
 		return
 	}
+
+	temperature := r.FormValue("temperature")
+	unit := r.FormValue("unit")
+
+	convertedTemperatures := convertTemperature(temperature, unit)
+
+	fmt.Fprintf(w, "Original temperature: %s\n", temperature)
+	fmt.Fprintf(w, "Converted temperatures:\n")
+	fmt.Fprintf(w, "Fahrenheit: %f\n", convertedTemperatures.Fahrenheit)
+	fmt.Fprintf(w, "Celsius: %f\n", convertedTemperatures.Celsius)
+	fmt.Fprintf(w, "Kelvin: %f\n", convertedTemperatures.Kelvin)
+
+}
+
+func convertTemperature(temperature string, unit string) Temperature {
+	// Implementa las conversiones de temperatura aquí según tus necesidades.
+	// Puedes utilizar fórmulas o funciones específicas de Go para realizar las conversiones.
+
+	// Por ejemplo:
+	// Si la unidad de origen es Celsius, realiza las conversiones correspondientes.
+	// Si la unidad de origen es Fahrenheit, realiza las conversiones correspondientes.
+	// Si la unidad de origen es Kelvin, realiza las conversiones correspondientes.
+
+	// Retorna una estructura Temperature con los valores convertidos.
+	return Temperature{
+		Fahrenheit: 0.0,
+		Celsius:    0.0,
+		Kelvin:     0.0,
+	}
+}
+
+type Temperature struct {
+	Fahrenheit float64
+	Celsius    float64
+	Kelvin     float64
 }
