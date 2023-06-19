0ed2304fc919c384172079c0d517c1f65e35e218
Santiago <vazquezgrillo.santiago@gmail.com>
2023-06-19 18:11:39 -0300

    fix errors

--- ConvertTemperature/main.go
+++ ConvertTemperature/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -13,30 +14,54 @@ func main() {
 
 func convertHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		http.Error(w, "Mehotd not allowed", http.StatusMethodNotAllowed)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
 	temperature := r.FormValue("temperature")
 	unit := r.FormValue("unit")
 
-	convertedTemperatures := convertTemperature(temperature, unit)
+	convertedTemperatures, err := convertTemperature(temperature, unit)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
 	fmt.Fprintf(w, "Original temperature: %s\n", temperature)
 	fmt.Fprintf(w, "Converted temperatures:\n")
 	fmt.Fprintf(w, "Fahrenheit: %f\n", convertedTemperatures.Fahrenheit)
 	fmt.Fprintf(w, "Celsius: %f\n", convertedTemperatures.Celsius)
 	fmt.Fprintf(w, "Kelvin: %f\n", convertedTemperatures.Kelvin)
-
 }
 
-func convertTemperature(temperature string, unit string) Temperature {
+func convertTemperature(temperature string, unit string) (Temperature, error) {
+	temp, err := strconv.ParseFloat(temperature, 64)
+	if err != nil {
+		return Temperature{}, err
+	}
 
-	// Retorna una estructura Temperature con los valores convertidos.
-	return Temperature{
-		Fahrenheit: 0.0,
-		Celsius:    0.0,
-		Kelvin:     0.0,
+	switch unit {
+	case "Fahrenheit":
+		return Temperature{
+			Fahrenheit: temp,
+			Celsius:    fahrenheitToCelsius(temp),
+			Kelvin:     celsiusToKelvin(fahrenheitToCelsius(temp)),
+		}, nil
+	case "Celsius":
+		return Temperature{
+			Fahrenheit: celsiusToFahrenheit(temp),
+			Celsius:    temp,
+			Kelvin:     celsiusToKelvin(temp),
+		}, nil
+	case "Kelvin":
+		return Temperature{
+			Fahrenheit: celsiusToFahrenheit(kelvinToCelsius(temp)),
+			Celsius:    kelvinToCelsius(temp),
+			Kelvin:     temp,
+		}, nil
+	default:
+		return Temperature{}, fmt.Errorf("Unrecognized unit: %s", unit)
 	}
 }
 
