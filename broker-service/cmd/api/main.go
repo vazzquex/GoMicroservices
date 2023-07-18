d7aed5afd085c825046fa351c80afb39ef29a6a3
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-07-17 22:32:06 -0300

    add broker service

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -3,27 +3,23 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 )
 
-const webPort = "80"
+const webPort = "8080"
 
-type Config struct {}
+type Config struct{}
 
 func main() {
 	app := Config{}
 
 	log.Printf("Starting broker service on port %s\n", webPort)
 
-	// define http server
-	srv := &http.Server{
-		Addr: fmt.Sprintf(":%s", webPort),
-		Handler: app.routes(),
-	}
+	router := app.routes()
 
-	// start the server
-	err := srv.ListenAndServe()
+	//start server
+	err := router.Run(fmt.Sprintf(":%s", webPort))
 	if err != nil {
 		log.Panic(err)
 	}
+
 }
--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -1,28 +1,35 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func (app *Config) routes() *gin.Engine {
+
 	router := gin.Default()
 
-	//specify who is allowed to connect
+	// specify who is allowed to connect
 	config := cors.DefaultConfig()
-
 	config.AllowOrigins = []string{"https://*", "http://*"}
 	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
-	config.AllowHeaders = []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Tocken"}
+	config.AllowHeaders = []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"}
 	config.ExposeHeaders = []string{"Link"}
+	config.AllowCredentials = true
 	config.MaxAge = 300
-
 	router.Use(cors.New(config))
 
 	router.POST("/", func(c *gin.Context) {
+		app.Broker(c.Writer, c.Request)
+	})
+
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+		log.Print("Pong")
 	})
 
 	return router
