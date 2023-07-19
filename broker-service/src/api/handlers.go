7d1f1d7c4aa1c416e6d448174b531964b8d7a14b
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-07-19 17:24:19 -0300

    add HandleSubmission in handlers.go

--- broker-service/src/api/handlers.go
+++ broker-service/src/api/handlers.go
@@ -1,8 +1,19 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 )
+
+type requestPayLoad struct {
+	Action string      `json:"action"`
+	Auth   AuthPayload `json:"auth, omitempty"`
+}
+
+type AuthPayload struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 
@@ -13,4 +24,29 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
 
+}
+
+func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
+	var requestPlyload requestPayLoad
+
+	err := app.readJSON(w, r, &requestPlyload)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	switch requestPlyload.Action {
+	case "auth":
+
+	default:
+		app.errorJSON(w, errors.New("unknown action"))
+	}
+}
+
+func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
+	// create some json we will send to the auth microservice
+
+	//call microservice
+
+	//make sure we get back the correct status code
 }
