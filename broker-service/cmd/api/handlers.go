6d3f62800aaeeb501ffe8c7b3bbd632d9cc14e8f
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-07-18 21:57:25 -0300

    add makefile

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,15 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
-
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 
@@ -18,13 +11,6 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Message: "Request received from the broker",
 	}
 
-	if payload.Error {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+	_ = app.writeJSON(w, http.StatusOK, payload)
 
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
 }
